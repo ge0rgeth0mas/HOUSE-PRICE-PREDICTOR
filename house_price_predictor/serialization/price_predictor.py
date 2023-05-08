8445f85cfded05096d851dedf27b9fee71cfe52a
ge0rgeth0mas <georgethomas1912@gmail.com>
2023-05-09 00:54:06 +0530

    Changed model serialization after target encoding

--- house_price_predictor/serialization/price_predictor.py
+++ house_price_predictor/serialization/price_predictor.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 import os
-from data_engineering.loaddata_fromdb import *
+from house_price_predictor.data_engineering.loaddata_fromdb import *
 
 # Import Data to Dataframe
 melb_house_prices = get_df('melb_data')
 
-# Extract Target foring from Data
+# Extract Target column from Data
 X = melb_house_prices.drop('price', axis=1)
 y = melb_house_prices.price
 
@@ -16,59 +16,37 @@ y = melb_house_prices.price
 X['date'] = pd.to_datetime(X['date'], format='%d/%m/%Y')
 X['year_sold'] = X['date'].dt.year
 
-# Fill null values in Car Spot
-X['car']=X['car'].fillna(0)
-
 # Convert landsize to landsize_log
 X['landsize_log'] = pd.Series(np.log(X['landsize']+1))
 
-# Remove data for houses built before 1800
-indices_yearbefore1800 = X[X['yearbuilt']<1800].index
-X = X.drop(indices_yearbefore1800)
-y = y.drop(indices_yearbefore1800)
+# Target type, regionname and SellerG features
+from category_encoders import TargetEncoder
+TargetEncodeCols = ['type', 'regionname', 'sellerg']
+te = TargetEncoder(cols=TargetEncodeCols)
+X = te.fit_transform(X, y)
 
-# Add column to mention which rows have missing yearbuilt
-X['yearbuilt_present'] = X['yearbuilt'].notnull().astype(int)
-# Fill missing yearbuilt values with mean
-mean_yearbuilt = X['yearbuilt'].mean()
-X['yearbuilt']=X['yearbuilt'].fillna(mean_yearbuilt)
-X['yearbuilt']=X['yearbuilt'].astype(int)
+X[TargetEncodeCols] /= 100000
 
-# Change names of sellers with less than 100 sales to 'other'
-min_sale = 100
-X['sellerg'] = X['sellerg'].apply(lambda x : x.lower() if 
-                                              (X['sellerg'].value_counts()[x]>min_sale)
-                                              else 
-                                              "other")
+# create dictionary of suburb and property counts
+suburb_pc_df= X.groupby(['suburb'])['propertycount'].mean() # create a series with each uniqiue suburb as index and its corresponding property count. Both mean() and size() can be used as each suburb has a fixed propertycount
+suburb_pc_dict = suburb_pc_df.to_dict() # use the series to create a dictonary
 
-# One Hot encode type and SellerG features
-from sklearn.preprocessing import OneHotEncoder
-OH_encoder = OneHotEncoder(handle_unknown='ignore', sparse_output=False, drop='first')
-OneHotCols =['type', 'sellerg']
-OH_cols= pd.DataFrame(OH_encoder.fit_transform(X[OneHotCols]))
-OH_cols.index = X.index # Indices were lost, add it back
-
-OH_col_names = OH_encoder.get_feature_names_out(OneHotCols)
-OH_col_names = [s.split('_')[-1] for s in OH_col_names]
-OH_cols = OH_cols.rename(columns=dict(zip(OH_cols.columns, OH_col_names)))
-
-X = pd.concat([X, OH_cols], axis=1)
 
 # Remove unnecessary features
-features_to_remove = ['suburb', 'address', 'type', 'method', 'sellerg', 'date', 
-                      'postcode', 'bedroom2', 'landsize', 'buildingarea', 
-                      'councilarea', 'regionname', 'propertycount']
+features_to_remove = ['suburb', 'address', 'method', 'date', 
+                        'postcode', 'bedroom2', 'car', 'landsize',
+                        'buildingarea', 'yearbuilt', 'councilarea']
 X.drop(features_to_remove, axis=1, inplace=True)
 
-# Train Random Forest Regressor model with n_estimators=375, max_features=21
+# Train Random Forest Regressor model with n_estimators=450, max_features=4
 from sklearn.ensemble import RandomForestRegressor
-model = RandomForestRegressor(n_estimators=375, max_features=21, random_state=1)
+model = RandomForestRegressor(n_estimators=450, max_features=4, random_state=1)
 model.fit(X, y)
 
 # Export trained model to pickle file
 import pickle
 
-path = os.getcwd()+'/resources/'
+path = os.getcwd()+'/house_price_predictor/resources/'
 pickle_filename = 'house_price_predictor.pickle'
 try:
     with open(path+pickle_filename, 'wb') as f:
@@ -91,12 +69,13 @@ try:
 except Exception as e:
     print("Error while saving columns to {} : {}".format(columns_filename, e))
 
-meanyear_filename = "meanyear.json"
-meanyear = {'meanyear' : mean_yearbuilt}
+# Export suburb_propertycount_dict
+
+dictionary_filename = "propertycount_persuburb.json"
 
 try:
-    with open(path+meanyear_filename, "w") as f:
-        f.write(json.dumps(meanyear))
-    print("Mean year saved successfully")
+    with open(path+dictionary_filename, "w") as f:
+        json.dump(suburb_pc_dict, f)
+    print("Dictionary saved successfully")
 except Exception as e:
-    print("Error while saving mean year to {} : {}".format(meanyear_filename, e))
+    print("Error while saving Dictionary to {} : {}".format(dictionary_filename, e))
